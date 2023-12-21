25906e85820ff42356000f2c8680f65968f298e3
alif0013 <mdalifh36@gmail.com>
2023-12-22 00:16:30 +0600

    added task route and post data to the data base successfully

--- src/Pages/UserDashboard/MyProfile.jsx
+++ src/Pages/UserDashboard/MyProfile.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { SlBadge } from "react-icons/sl";
+import { Link } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+// import useMyPost from '../../../hooks/useMyPost';
 
 const MyProfile = () => {
+
+    const { user } = useAuth()
+
+    // const [myPost, refetch] = useMyPost()
+
     return (
         <div>
-            my profile
+            <Helmet>
+                <title>Dashboard | My Profile</title>
+            </Helmet>
+
+            <h2 className='text-4xl font-bold text-center mt-10'>My Profile</h2><hr className='border-2 mt-4 md:w-[400px] mx-auto' />
+
+            <div className='flex justify-center mt-10 lg:mt-20'>
+
+                <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                    <div className="flex flex-col items-center pb-10">
+                        <img className="w-24 h-24 mb-3 mt-2 rounded-full shadow-lg" src={user?.photoURL} alt="Bonnie image" />
+                        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Name: {user?.displayName}</h5>
+                        <div className="badge badge-info gap-2 my-1">
+                            <SlBadge></SlBadge>
+                            Bronze
+                        </div>
+
+                        <span className="text-sm text-gray-500 dark:text-gray-400">Email: {user?.email}</span>
+                        <div className="flex mt-4 md:mt-6">
+                            <Link to='/dashboard/newtask' className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Task</Link>
+                            <Link to='/dashboard/managetask' className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3">Manage Task</Link>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     );
 };
--- src/Pages/UserDashboard/NewTask.jsx
+++ src/Pages/UserDashboard/NewTask.jsx
@@ -1,10 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useAuth from '../../hooks/useAuth';
+import useAxiosPublic from '../../hooks/useAxiosPublic';
+import { Helmet } from 'react-helmet-async';
+import toast from 'react-hot-toast';
 
 const NewTask = () => {
+    const { user } = useAuth();
+    const [selectedTags, setSelectedTags] = useState(''); // Initialize state for the selected level
+    const axiosPublic = useAxiosPublic()
+    // const [ , refetch] = usePosts()
+
+    const handleAddPost = e => {
+        e.preventDefault();
+        const form = e.target;
+        const title = form.title.value;
+        const description = form.description.value;
+        const email = user.email;  //useAuth theke asa user email
+        const name = user.displayName;  //useAuth theke asa user email
+        const userImg = user.photoURL;
+        const tags = selectedTags; // Use the selectedLevel from state
+        const deadline = form.deadline.value;
+        // Get the current time and format it as "hr:min:sec"
+        // const currentTime = new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric' });
+        // Get the current time and format it as "hr:min"
+        const currentTime = new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric' });
+
+        // Get the current date and format it as "YYYY-MM-DD"
+        const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+        const date = new Date().toLocaleDateString('en-US', options);
+
+
+        const newTask = { title, currentTime, userImg, date, description, email, name,  tags, deadline }
+        // console.log(newPost);
+        axiosPublic.post('/task', newTask)
+            .then(res => {
+                console.log(res.data);
+                if (res.data.insertedId) {
+                    toast.success('Task added Successfully!')
+
+                    //refetch post to update the data 
+                    // refetch();
+                }
+            })
+    }
+
+
+    const handleSelectTags = e => {
+        setSelectedTags(e.target.value); // Update selectedLevel state when the select value changes
+    };
+
+
+
     return (
         <div>
-            new task
+        <Helmet>
+            <title>Dashboard | New Task</title>
+        </Helmet>
+
+        <h2 className='text-4xl font-bold text-center mt-10'>Create a New Task</h2><hr className='border-2 mt-4 md:w-[400px] mx-auto' />
+
+        <div className='px-4 rounded'>
+            <div className='text-center rounded p-20 bg-[#F0F0F0] mt-10'>
+
+                <form onSubmit={handleAddPost}>
+                    <div className='mt-3'>
+                        <input type="text" name='title' placeholder="Title" className="input input-bordered w-full max-w-xs mr-5 mb-3" />
+                        <input type="text" name='description' placeholder="Description" className="input input-bordered w-full max-w-xs" />
+
+                    </div>
+                    
+                    <div className='mt-3'>
+                        {/* <input type="text" name='level' placeholder="Easy, Medium, Hard" className="input input-bordered w-full max-w-xs mr-5 mb-3" /> */}
+                        <select
+                            className="select input select-bordered w-full max-w-xs mr-5"
+                            onChange={handleSelectTags} // Handle select change
+                            value={selectedTags} // Set the selected value from state
+                        >
+                            <option value="" disabled selected>Priority Level</option>
+                            <option value="high">High</option>
+                            <option value="moderate">Moderate</option>
+                            <option value="low">Low</option>
+
+                        </select>
+
+                        <input type="text" name='deadline' placeholder="Deadline" className="input input-bordered w-full max-w-xs" />
+
+                    </div>
+
+                    <div className='mt-3'>
+
+
+                    </div>
+
+                    <button type='submit' className="btn md:w-[660px] text-white text-center bg-gradient-to-r from-[#4A00E0] to-blue-500 mt-6">Create Task</button>
+
+
+                </form>
+
+
+            </div>
         </div>
+    </div>
     );
 };
 
